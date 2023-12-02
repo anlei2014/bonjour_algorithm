50f843a1018c78fee1e19c62761faa7072b5a83c
anlei <404280116@qq.com>
2023-12-02 22:20:02 +0800

    5

---
+++ List/lc_203_移除链表元素/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        // 删除头结点
+        while (head != NULL && head->val == val) { // 注意这里不是if
+            ListNode* tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+
+        // 删除非头结点
+        ListNode* cur = head;
+        while (cur != NULL && cur->next!= NULL) {
+            if (cur->next->val == val) {
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+            } else {
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
+void printList(ListNode* head)
+{
+    while (head != nullptr)
+    {
+        std::cout << head->val << std::endl;
+        head = head->next;
+    }
+}
+
+int main() {
+
+    /* 初始化链表 1 -> 3 -> 2 -> 5 -> 4 */
+    // 初始化各个节点
+    ListNode* n0 = new ListNode(1);
+    ListNode* n1 = new ListNode(2);
+    ListNode* n2 = new ListNode(6);
+    ListNode* n3 = new ListNode(3);
+    ListNode* n4 = new ListNode(4);
+    ListNode* n5 = new ListNode(5);
+    ListNode* n6 = new ListNode(6);
+
+    // 构建节点之间的引用
+    n0->next = n1;
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+    n5->next = n6;
+    printList(n0);
+    std::cout << "--------------------" << std::endl;
+    Solution s;
+    n0 = s.removeElements(n0,6);
+    printList(n0);
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
